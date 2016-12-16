811f88e8ee969a300438321193c9f52ec0cb8fe4
Rain Qian <rain@yunio.com>
2016-12-16 18:43:14 +0800

    swift test

--- www/cordova_plugin_sw.js
+++ www/cordova_plugin_sw.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "cordova-plugin-sw", "greet", [name]);
+        cordova.exec(successCallback, errorCallback, "cordova_plugin_sw", "greet", [name]);
     }
 };
